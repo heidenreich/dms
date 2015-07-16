14b438731096458ce30b8b57ea79757ea4822f6d
Todd Heidenreich <jtheidenreich@gmail.com>
2015-07-16 12:20:56 -0400

    git on in it

---
+++ app/scripts/main.js
@@ -0,0 +1,20 @@
+/* jshint devel:true */
+console.log('DMS');
+
+$(document).ready(function(){
+
+	$('.testimonial-slider').slick({
+		  dots: true,
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 1,
+	  variableHeight: true,
+	  centerPadding: '100px',
+	});
+
+
+
+
+
+
+});
